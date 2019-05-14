bd8f305a78b651489827d395ae78e28e09b04ec0
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2019-05-14 10:01:26 -0400

    add positioning for mario, fix background image file path

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $(".mario").on("click", function() {
     // prompt("what is marios favourite game?");
-    $(".mario")
-      .css({ left: "93%", top: "40%" })
-      .delay(5000);
-    // $(".background")
-    //   .delay(800)
-    //   .toggleClass("animated fadeOut");
+    $(".mario").css({ left: "93%", top: "40%" });
+
+    $(".background").toggleClass("animated fadeOut delay-1s");
   });
 });
+// });
