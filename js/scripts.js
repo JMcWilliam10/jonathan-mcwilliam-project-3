0332ac5eea9c514255fd46a30c0353e4cceb95a8
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2019-05-14 19:54:35 -0400

    begin adding level to level functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
+let incrementor = 0;
+let levels = ["lvlOne", "lvlTwo"];
+let levelTwo = "(./../../assets/images/lvlOne.png)";
+
+class Level;
+
 $(document).ready(function() {
   $(".mario").on("click", function() {
     // prompt("what is marios favourite game?");
-    $(".mario").css({ left: "93%", top: "40%" });
-
-    $(".background").toggleClass("animated fadeOut delay-1s");
+  });
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+    let userChoice = $("select").val();
+    if (userChoice === "flex-end") {
+      incrementor++;
+      $(".scoreValue").html(incrementor);
+      $(".mario").css({ left: "93%", bottom: "38.5%" });
+      $(".background").toggleClass("animated fadeOut delay-1s");
+      setTimeout(function() {
+        $(".background").toggleClass("animated fadeOut delay-1s");
+        $(".wrapper").css("background-position", "10% 0");
+        $(".mario").css({ left: "15%" });
+      }, 3000);
+    } else {
+      $(".mario").css({ left: "93%", bottom: "38.5%" });
+      $(".background").toggleClass("animated fadeOut delay-1s");
+      setTimeout(function() {
+        $(".background").toggleClass("animated fadeOut delay-1s");
+        $(".wrapper").css("background-position", "10% 0");
+        $(".mario").css({ left: "15%" });
+      }, 3000);
+    }
   });
 });
-// });
